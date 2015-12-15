a728b1a6ac07ed3acc052af38fea43b01ad88cc7
Mark Dorison <mark@chromatic.is>
2015-12-15 23:49:20 +0000

    Replaced manually entity loading with upcasting.

--- src/Controller/RecurlyPushListenerController.php
+++ src/Controller/RecurlyPushListenerController.php
@@ -1,4 +1,5 @@
-<?php /**
+<?php
+/**
  * @file
  * Contains \Drupal\recurly\Controller\RecurlyPushListenerController.
  */
--- src/Routing/RecurlySubscriptionRoutes.php
+++ src/Routing/RecurlySubscriptionRoutes.php
@@ -29,30 +29,33 @@ class RecurlySubscriptionRoutes {
     // Add and configure the new route.
     $routes = [];
     $routes['recurly.subscription_list'] = new Route(
-      "/$entity_type/{entity_id}/subscription",
+      "/$entity_type/{entity}/subscription",
       [
         '_controller' => '\Drupal\recurly\Controller\RecurlySubscriptionListController::subscriptionList',
         '_title' => 'Subscription Information',
       ],
-      ['_permission' => 'administer recurly']
+      ['_permission' => 'administer recurly'],
+      ['parameters' => ['entity' => ['type' => 'entity:' . $entity_type]]]
     );
 
     $routes['recurly.subscription_signup'] = new Route(
-      "/$entity_type/{entity_id}/subscription/signup",
+      "/$entity_type/{entity}/subscription/signup",
       [
         '_controller' => '\Drupal\recurly\Controller\RecurlySubscriptionSelectPlanController::planSelect',
         '_title' => \Drupal::config('recurly.settings')->get('recurly_subscription_max') === '1' ? 'Signup' : 'Add plan',
       ],
       // @FIXME: Permissions callback logic needs to be implemented.
-      ['_permission' => 'administer recurly']
+      ['_permission' => 'administer recurly'],
+      ['parameters' => ['entity' => ['type' => 'entity:' . $entity_type]]]
     );
     $routes['recurly.subscription_change'] = new Route(
-      "/$entity_type/{entity_id}/subscription/change",
+      "/$entity_type/{entity}/subscription/change",
       [
         '_controller' => '\Drupal\recurly\Controller\RecurlySubscriptionSelectPlanController::planSelect',
         '_title' => 'Change Plan',
       ],
-      ['_permission' => 'administer recurly']
+      ['_permission' => 'administer recurly'],
+      ['parameters' => ['entity' => ['type' => 'entity:' . $entity_type]]]
     );
 
     return $routes;
