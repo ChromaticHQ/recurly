44d722983947933351f2b679647a409d23532dc4
adamzimmermann <adamzimmermann@1152646.no-reply.drupal.org>
2017-03-27 19:53:14 +0000

    Issue #2626662 by adamzimmermann, walangitan, eojthebrave: Add recurlyjs form validation

--- modules/recurlyjs/recurly-element.js
+++ modules/recurlyjs/recurly-element.js
@@ -30,8 +30,8 @@ Drupal.recurly.recurlyJSTokenFormSubmit = function(event) {
   event.preventDefault();
 
   // Reset the errors display
-  $('#recurly-form-errors').html('');
-  $('input').removeClass('error');
+  Drupal.recurly.clearErrors();
+  $('.recurlyjs-form-item__error').removeClass('.recurlyjs-form-item__error');
 
   // Disable the submit button
   $('button').attr('disabled', true);
@@ -51,19 +51,93 @@ Drupal.recurly.recurlyJSTokenFormSubmit = function(event) {
  * Handles form errors.
  */
 Drupal.recurly.recurlyJSFormError = function(err) {
+  var errorFields = {
+    first_name: Drupal.t('First name'),
+    last_name: Drupal.t('Last name'),
+    email: Drupal.t('Email address'),
+    number: Drupal.t('Credit Card number'),
+    postal_code: Drupal.t('Postal Code'),
+    month: Drupal.t('Expiration Month'),
+    year: Drupal.t('Expiration Year')
+  };
+
   $('button').attr('disabled', false);
 
-  // Add the error class to all form elements that returned an error.
-  if (typeof err.fields !== 'undefined') {
-    $.each(err.fields, function (index, value) {
-      $('input[data-recurly="' + value + '"]').addClass('error');
-    });
-  }
+  var fieldErrorsList = $.map(err.fields, function (field) {
+    // Add a class to each element with an error.
+    $('.recurlyjs-form-item__' + field).addClass('recurlyjs-form-item__error');
+    // Append a message to the list.
+    return '<li>'+ errorFields[field] +'</li>';
+  }).join('');
 
   // Add the error message to the form within standard Drupal message markup.
   if (typeof err.message !== 'undefined') {
-    var messageMarkup = '<div class="messages error">' + err.message + '</div>';
+    var messageMarkup = '<div class="messages error">' + err.message + '<ul>' + fieldErrorsList + '</ul></div>';
     $('#recurly-form-errors').html(messageMarkup);
+  }
+};
+
+Drupal.recurly.clearErrors = function() {
+  $('#recurly-form-errors').html('');
+};
+
+/**
+ * Update icon used in credit card number field when value of field changes.
+ *
+ * @param state
+ *   See https://dev.recurly.com/docs/events
+ */
+Drupal.recurly.recurlyJSPaymentMethod = function(state) {
+if (state.fields.number.focus == true) {
+  $('.recurlyjs-icon-card').removeClass().addClass('recurlyjs-icon-card recurlyjs-icon-card__' + state.fields.number.brand);
+}
+};
+
+/**
+ * Update order summary when pricing information changes.
+ *
+ * @param pricingState
+ *   See https://dev.recurly.com/docs/pricing
+ */
+Drupal.recurly.recurlyJSPricing = function(pricingState) {
+  Drupal.recurly.clearErrors();
+
+  var displaySubtotal = false;
+  // Toggle visibility of order summary elements that may or may not be used
+  // depending on the plan.
+  // Toggle visiblity of setup fee.
+  if (parseInt(pricingState.now.setup_fee) > 0) {
+    $('.recurlyjs-setup-fee').removeClass('recurlyjs-element__hidden');
+    displaySubtotal = true;
+  }
+  else {
+    $('.recurlyjs-setup-fee').addClass('recurlyjs-element__hidden');
+  }
+
+  // Toggle visibility of discount.
+  if (parseInt(pricingState.now.discount) > 0) {
+    $('.recurlyjs-discount').removeClass('recurlyjs-element__hidden');
+    displaySubtotal = true;
+  }
+  else {
+    $('.recurlyjs-discount').addClass('recurlyjs-element__hidden');
+  }
+
+  // Toggle visibility of sub total. No need to display sub-total if none of the
+  // above are set.
+  if (displaySubtotal && parseInt(pricingState.now.subtotal) > 0) {
+    $('.recurlyjs-subtotal').removeClass('recurlyjs-element__hidden');
+  }
+  else {
+    $('.recurlyjs-subtotal').addClass('recurlyjs-element__hidden');
+  }
+
+  // Toggle visibility of taxes.
+  if (parseInt(pricingState.now.tax) > 0) {
+    $('.recurlyjs-tax').removeClass('recurlyjs-element__hidden');
+  }
+  else {
+    $('.recurlyjs-tax').addClass('recurlyjs-element__hidden');
   }
 };
 
@@ -76,17 +150,33 @@ Drupal.behaviors.recurlyJSConfigureForm = {
       publicKey: settings.public_key,
       style: {
         number: {
-          placeholder: 'Credit Card Number'
+          placeholder: 'xxxx xxxx xxxx xxxx'
         },
         month: {
-          placeholder: 'Month (mm)'
+          placeholder: 'MM'
         },
         year: {
-          placeholder: 'Year (yy)'
+          placeholder: 'YYYY'
         },
         cvv: {
-          placeholder: 'Security Code',
+          placeholder: '123'
         }
+      }
+    });
+
+    // Add listener for changes to credit card field's value.
+    recurly.on('change', Drupal.recurly.recurlyJSPaymentMethod);
+
+    // Create a new pricing module, and track change events for pricing related
+    // elements on the form.
+    var pricing = recurly.Pricing();
+    pricing.attach($('#recurlyjs-subscribe-form'));
+    pricing.on('change', Drupal.recurly.recurlyJSPricing);
+    pricing.on('error', function (error) {
+      // Checking for coupon validation errors.
+      if (typeof error.message !== 'undefined') {
+        var messageMarkup = '<div class="messages error coupon">' + error.message + '</div>';
+        $('#recurly-form-errors').html(messageMarkup);
       }
     });
   }
