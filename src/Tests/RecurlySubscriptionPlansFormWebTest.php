8b2e008e279247c0deff0f3f4baf75d2412f0783
markdorison <markdorison@346106.no-reply.drupal.org>
2017-04-21 17:48:47 +0000

    Issue #2870196 by markdorison: Add RecurlySubscriptionPlansFormWebTest

---
+++ src/Tests/RecurlySubscriptionPlansFormWebTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\recurly\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests recurly subscription plans form.
+ *
+ * @group recurly
+ */
+class RecurlySubscriptionPlansFormWebTest extends WebTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['recurly'];
+
+  /**
+   * Admin user.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $adminUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $permissions = [
+      'administer recurly',
+    ];
+    $this->adminUser = $this->createUser($permissions);
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Test settings form submission.
+   */
+  protected function testPlanFormWithoutCredentials() {
+    $this->drupalGet('/admin/config/services/recurly/subscription-plans');
+    $this->assertResponse(200);
+    $this->assertText('Could not initialize the Recurly client.');
+  }
+
+}
