5a5cf0bf57e5bf454cb3f340a12ecd7d1b27c512
markdorison <markdorison@346106.no-reply.drupal.org>
2017-04-21 17:48:22 +0000

    Issue #2870196 by markdorison: Add tests for RecurlySettingsFormWebTest

--- src/Tests/RecurlySettingsFormWebTest.php
+++ src/Tests/RecurlySettingsFormWebTest.php
@@ -45,39 +45,61 @@ class RecurlySettingsFormWebTest extends WebTestBase {
     $this->drupalGet('/admin/config/services/recurly');
     $this->assertResponse(200);
 
+    $this->assertNoDuplicateIds();
+
+    $this->assertText(t('Default account settings'));
     $this->assertText(t('Push notification settings'));
     $this->assertText(t('Built-in subscription/invoice pages'));
   }
 
   /**
-   * Test for the presence of the settings fields.
+   * Test account settings fields.
    */
-  protected function testSettingsFields() {
+  protected function testAccountSettingsFields() {
     $this->drupalGet('/admin/config/services/recurly');
+    $this->assertFieldById('edit-recurly-private-api-key');
+    $this->assertFieldById('edit-recurly-public-key');
+    $this->assertFieldById('edit-recurly-subdomain');
+    $this->assertFieldByName('recurly_default_currency');
+    $this->assertOptionSelected('edit-recurly-default-currency', 'USD', 'Currency field defaults to USD.');
+  }
 
-    // Account settings group.
-    $this->assertText(t('Default account settings'));
-    $this->assertField('recurly_private_api_key');
-    $this->assertField('recurly_public_key');
-    $this->assertField('recurly_subdomain');
-    $this->assertField('recurly_default_currency');
+  /**
+   * Test push notification settings fields.
+   */
+  protected function testPushNotificationSettingFields() {
+    $this->drupalGet('/admin/config/services/recurly');
+    $this->assertFieldById('edit-recurly-listener-key');
+    $this->assertFieldById('edit-recurly-push-logging');
+  }
 
-    // Push notifcation group.
-    $this->assertText(t('Push notification settings'));
-    $this->assertField('recurly_listener_key');
-    $this->assertField('recurly_push_logging');
+  /**
+   * Test account sync settings fields.
+   */
+  protected function testAccountSyncSettingsFields() {
+    $this->drupalGet('/admin/config/services/recurly');
+    $this->assertFieldByName('recurly_entity_type');
+    $this->assertOptionSelected('edit-recurly-entity-type', 'user', 'Entity type defaults to user.');
+    $this->assertFieldById('edit-recurly-token-mapping-email', '[user:mail]', 'Email token field is present and contains default value: [user:mail].');
+    $this->assertFieldById('edit-recurly-token-mapping-username', '[user:name]', 'Username token field is present and contains default value: [user:name].');
+  }
 
-    // Account sync group.
-    $this->assertField('recurly_entity_type');
-
-    // Buil-in page group.
+  /**
+   * Test Recurly pages settings fields.
+   */
+  protected function testRecurlyPagesSettingsFields() {
+    $this->drupalGet('/admin/config/services/recurly');
     $this->assertField('recurly_pages');
-    $this->assertField('recurly_coupon_page');
-    $this->assertField('recurly_subscription_display');
-    $this->assertField('recurly_subscription_max');
-    // $this->assertField('recurly_subscription_upgrade_timeframe');
-    // $this->assertField('recurly_subscription_downgrade_timeframe');.
-    $this->assertField('recurly_subscription_cancel_behavior');
+    $this->assertFieldChecked('edit-recurly-pages', 'Recurly pages enabled by default.');
+    $this->assertFieldByName('recurly_coupon_page');
+    $this->assertFieldChecked('edit-recurly-coupon-page', 'Recurly coupon pages enabled by default.');
+    $this->assertFieldByName('recurly_subscription_display');
+    $this->assertFieldChecked('edit-recurly-subscription-display-live', 'Recurly subscription display set to \'live\' by default.');
+    $this->assertFieldByName('recurly_subscription_max');
+    $this->assertFieldChecked('edit-recurly-subscription-max-1', 'Recurly set to single plan mode by default.');
+
+    $this->assertFieldByName('recurly_subscription_cancel_behavior');
+    $this->assertFieldChecked('edit-recurly-subscription-cancel-behavior-cancel', 'Cancel behavior defaults to "cancel".');
   }
 
   /**
