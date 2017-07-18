f92cbf3636b960eb180f9e087938cebd3f6302ac
markdorison <markdorison@346106.no-reply.drupal.org>
2017-07-18 19:03:49 +0000

    Issue #2895040 by NarendraR, markdorison: Unused placeholder @space in RecurlyFormatManger.php

--- src/RecurlyFormatManager.php
+++ src/RecurlyFormatManager.php
@@ -288,7 +288,11 @@ class RecurlyFormatManager {
       $amount = $this->formatCurrency($coupon_currency->amount_in_cents, $currency, $html);
     }
 
-    return Html::escape($coupon->name) . $this->stringTranslation->translate('@space(Coupon code: @coupon_code, @amount discount)', ['@space' => ' ', '@coupon_code' => $coupon->coupon_code, '@amount' => $amount]);
+    return Html::escape($coupon->name) . $this->stringTranslation->translate('@space(Coupon code: @coupon_code, @amount discount)', [
+      '@space' => ' ',
+      '@coupon_code' => $coupon->coupon_code,
+      '@amount' => $amount,
+    ]);
   }
 
 }
